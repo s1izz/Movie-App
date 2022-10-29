d759191e8c5d35b273cdc6dd595425778276d897
slizz <cadenneary@gmail.com>
2022-10-28 22:06:37 -0500

    Push 15

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,37 +1,40 @@
-import './MovieInfo.css'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Nav from './Nav'
-import StarIcon from '@mui/icons-material/Star';
+import "./MovieInfo.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Nav from "./Nav";
+import StarIcon from "@mui/icons-material/Star";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 function MovieInfo() {
-    const { id } = useParams()
-    const [movieInfo, setMovieInfo] = useState([])
-    const [rating, setRating] = useState()
+  const { id } = useParams();
+  const [movieInfo, setMovieInfo] = useState([]);
+  const [rating, setRating] = useState(5);
 
-    async function getMovieInfo() {
-      const { data } = await axios.get(`https://www.omdbapi.com/?i=${id}&apikey=79976878`).then(getRating())
-      // console.log(data)
-      setMovieInfo(data)
-      getRating()
-    }
+  async function getMovieInfo() {
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?i=${id}&apikey=79976878`
+    );
+    console.log(data);
+    setMovieInfo(data);
+    getRating(data);
+  }
 
-    function getRating() {
-      if (movieInfo.imdbRating > 5) {
-        setRating(movieInfo.imdbRating / 2)
-        console.log(rating)
-        return
-      }
-      setRating(movieInfo.imdbRating)
-    }
+  function getRating(data) {
+    setRating(data.imdbRating / 2);
+  }
 
-    useEffect(() => {
-      getMovieInfo()
-    }, [])
+  useEffect(() => {
+    getMovieInfo();
+  }, []);
+
+  useEffect(() => {
+    console.log(rating);
+  }, [rating]);
 
   return (
-    <div className='movieInfo'>
+    <div className="movieInfo">
       <Nav
         logo__classname="logo"
         span__color__classname="purple"
@@ -42,18 +45,33 @@ function MovieInfo() {
       />
       <div className="info__container">
         <div className="movieInfo__img--container">
-          <img className='movieInfo__img' src={movieInfo.Poster} alt="" />
+          <img className="movieInfo__img" src={movieInfo.Poster} alt="" />
         </div>
         <div className="movieInfo__container">
           <h1 className="movieInfo__title">{movieInfo.Title}</h1>
           <div className="movieInfo__rating">
-            {/* {new Array(Math.floor(rating)).fill(0)} */}
+            {/* {new Array(Math.floor(rating)).fill(0).map((_, index) => (
+              <StarIcon key={index} />
+            ))}
+            {!Number.isInteger(rating) && <StarHalfIcon />} */}
+            {new Array(Math.floor(rating)).fill(<StarBorderIcon />).map((_, index) => (
+              <StarIcon />
+            ))}
           </div>
-          {/* <button onClick={console.log(rating)}>rating</button> */}
+          <div className="movieInfo__points">
+            <div className="movieInfo__point">Year: {movieInfo.Year}</div>
+            <div className="movieInfo__point">Rated: {movieInfo.Rated}</div>
+            <div className="movieInfo__point">Type: {movieInfo.Type}</div>
+            <div className="movieInfo__point">Genre: {movieInfo.Genre}</div>
+            <div className="test">
+              {<StarBorderIcon /> || <StarIcon />}
+            </div>
+          </div>
         </div>
       </div>
+      {/* <button onClick={console.log(rating)}>rating</button> */}
     </div>
-  )
+  );
 }
 
-export default MovieInfo
+export default MovieInfo;
